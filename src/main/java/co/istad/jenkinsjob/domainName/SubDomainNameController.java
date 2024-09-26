8ed9070658eebf96fe1ff30ecb224349aa5ad788
ONSOBEN <sobenkh35@gmail.com>
2024-09-26 08:39:29 +0700

    Domain name system

---
+++ src/main/java/co/istad/jenkinsjob/domainName/SubDomainNameController.java
@@ -0,0 +1,25 @@
+package co.istad.jenkinsjob.domainName;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+public class SubDomainNameController {
+    private final SubDomainNameService subdomainService;
+    @PostMapping("/create-subdomain")
+    public String createSubdomain( @RequestParam String subdomain,
+                                  @RequestParam String ipAddress) {
+        return subdomainService.createSubdomain(subdomain, ipAddress);
+    }
+    @DeleteMapping("/delete-subdomain")
+    public String deleteSubdomain(@RequestParam String subdomain) {
+        return subdomainService.deleteSubdomainByName(subdomain);
+    }
+
+    @GetMapping("/get-records")
+    public String getRecords() {
+        return subdomainService.getAllDNSRecords();
+    }
+
+}
